e62e91280514f1702b80b47b449be9278bb2ab3b
Chris Chasm <chris@chasm.solutions>
2020-07-22 09:09:40 -0600

    Added plugin updated

--- static-section.php
+++ static-section.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Disciple Tools - Static Section
  * Description: This DT extension adds either a top tab of section to metrics and allows you to build iframe or html content into pages.
- * Version:  0.1.1
+ * Version:  0.1.2
  * Author URI: https://github.com/DiscipleTools
  * Requires at least: 4.7.0
  * (Requires 4.7+ because of the integration of the REST API at 4.7 and the security requirements of this milestone version.)
@@ -61,7 +61,7 @@ class Static_Section {
     public $token = 'dt_static_section';
     public $title = 'Static Section';
     public $permissions = 'manage_dt';
-    public $github_url = 'https://github.com/ZumeProject/disciple-tools-static-section';
+    public $github_url = 'https://github.com/DiscipleTools/disciple-tools-static-section';
 
     /**  Singleton */
     private static $_instance = null;
@@ -84,6 +84,31 @@ class Static_Section {
         if ( is_admin() ) {
             add_action( "admin_menu", [ $this, "register_menu" ] );
             add_filter( 'plugin_row_meta', [ $this, 'plugin_description_links' ], 10, 4);
+            // Check for plugin updates
+            if ( ! class_exists( 'Puc_v4_Factory' ) ) {
+                require( get_template_directory() . '/dt-core/libraries/plugin-update-checker/plugin-update-checker.php' );
+            }
+            /**
+             * Below is the publicly hosted .json file that carries the version information. This file can be hosted
+             * anywhere as long as it is publicly accessible. You can download the version file listed below and use it as
+             * a template.
+             * Also, see the instructions for version updating to understand the steps involved.
+             * @see https://github.com/DiscipleTools/disciple-tools-version-control/wiki/How-to-Update-the-Starter-Plugin
+             * @todo enable this section with your own hosted file
+             * @todo An example of this file can be found in /includes/admin/disciple-tools-starter-plugin-version-control.json
+             * @todo It is recommended to host this version control file outside the project itself. Github is a good option for delivering static json.
+             */
+
+            /***** @todo remove from here
+
+            $hosted_json = "https://raw.githubusercontent.com/DiscipleTools/disciple-tools-version-control/master/disciple-tools-starter-plugin-version-control.json"; // @todo change this url
+            Puc_v4_Factory::buildUpdateChecker(
+            $hosted_json,
+            __FILE__,
+            'disciple-tools-starter-plugin'
+            );
+
+             ********* @todo to here */
         }
 
         add_action( 'dt_top_nav_desktop', [ $this, 'top_nav_desktop' ], 50 );
